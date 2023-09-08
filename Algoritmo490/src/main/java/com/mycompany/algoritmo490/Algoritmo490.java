3bf8996c0ec743cfdab1d04ab8f7d19b2aaf141e
JheniJul <108576475+JheniJul@users.noreply.github.com>
2023-09-08 18:36:52 -0300

    Add files via upload

---
+++ Algoritmo490/src/main/java/com/mycompany/algoritmo490/Algoritmo490.java
@@ -0,0 +1,31 @@
+/*Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license */
+
+package com.mycompany.algoritmo490;
+
+import javax.swing.JOptionPane;
+
+/*@author sonha */
+
+public class Algoritmo490 {
+    public static void main(String[] args) {
+        int k;
+        String[] nome = new String[10];
+        
+        for (k = 0; k < 10; k++) {
+            nome[k] = JOptionPane.showInputDialog("Digite palavras em letras minusculas " + (k + 1) + "° : ");   
+            nome[k] = restantes(nome[k], "c");
+
+        }
+        
+        for (k = 0; k < 10; k++) {
+            System.out.println((k + 1) + " - " + nome[k] + "\n");
+        }
+    }
+    
+     public static String restantes(String texto, String x){
+        return texto.replace(x, "*");
+    }
+    
+}
+
+
